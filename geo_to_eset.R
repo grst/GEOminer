2a14592b9c518ae93e72548c99c596a928b890dd
Gregor Sturm <mail@gregor-sturm.de>
2016-10-21 09:05:00 +0200

    Adapt script to download also GSE

--- geo_to_eset.R
+++ geo_to_eset.R
@@ -12,17 +12,31 @@
 library(GEOquery)
 
 args = commandArgs(trailingOnly = TRUE)
-geo_id = args[1]
+geo.id = args[1]
 outdir = args[2]
-outfile = paste(outdir, "/", geo_id, ".Rdata", sep="")
-print(paste("GEO ID: ", geo_id))
+outfile = file.path(outdir, "%s.Rdata")
+print(paste("GEO ID: ", geo.id))
 print(paste("OUTFILE: ", outfile))
 
-gds = getGEO(geo_id)
-eset = GDS2eSet(gds)
+saveEset = function(eset, outfile) {
+  print(paste("Storing expression set to", outfile))
+  save(eset, file=outfile)
+} 
 
-print(paste("Storing expression set to", outfile))
-save(eset, file=outfile)
+geo.res = gds = getGEO(geo.id)
+if(grepl("^GDS", geo.id)) {
+  # GEO DataSet. Have to convert to eset first. 
+  eset = GDS2eSet(geo.res)
+  saveEset(eset, sprintf(outfile, geo.id))
+} else {
+  # GEO Series. Might contain multiple esets. 
+  for (i in seq_along(geo.res)) {
+    saveEset(geo.res[[i]], sprintf(outfile, paste(geo.id, i, sep="_")))
+  }
+}
+
+
+
 
 
 
